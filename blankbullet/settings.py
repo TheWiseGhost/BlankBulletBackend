8d04b7eded17806f09c09093346c9e8870cad374
Aditya Byju <byjuaditya@gmail.com>
2025-01-12 22:34:26 -0600

    Try whitenoise

--- blankbullet/settings.py
+++ blankbullet/settings.py
@@ -18,6 +18,7 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://trydropfast.vercel.app
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
